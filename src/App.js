9c76b4fc27c67adcee12c80558850c827b535658
Sanjoy9800 <sanjurudra4@gmail.com>
2023-12-16 21:15:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Body from "./components/Body/Body";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Body />
+    </div>
+  );
+}
+
+export default App;
